edcae382ca4dcac55ad167e7c2a93b8f2ea44a4e
mfrancis95 <mikefrancis95@gmail.com>
2020-09-08 05:29:26 -0400

    Add starter code for web service

---
+++ services/web/src/App.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+const TASKS_API = `${process.env.REACT_APP_TASKS_API_HOST}:${process.env.REACT_APP_TASKS_API_PORT}/api/v1`;
+
+export default class App extends React.Component {
+    
+    constructor(properties) {
+        super(properties);
+        this.state = {tasks: []};
+    }
+    
+    componentDidMount() {
+        this.getTasks();
+    }
+    
+    addTask = async (_id) => {
+        const data = await fetch(`${TASKS_API}/task/${_id}`)
+            .then(response => response.json());
+        if (data.success) {
+            let tasks = this.state.tasks;
+            tasks.unshift(data.task);
+            this.setState({tasks: tasks});
+        }
+    }
+    
+    deleteTask = async (_id) => {
+        const data = await fetch(`${TASKS_API}/task/${_id}`, {method: 'DELETE'})
+            .then(response => response.json());
+        if (data.success) {
+            let tasks = this.state.tasks;
+            tasks.splice(tasks.findIndex(task => task._id === _id), 1);
+        }
+    }
+    
+    getTasks = async (filters = {}) => {
+        let route = `${TASKS_API}/tasks?`;
+        if (filters.completed) {
+            route += 'completed=true&'
+        }
+        if (filters.dueToday) {
+            route += 'dueToday=true&'
+        }
+        if (filters.dueTomorrow) {
+            route += 'dueTomorrow=true&';
+        }
+        if (filters.overdue) {
+            route += 'overdue=true';
+        }
+        console.log(route);
+        const data = await fetch(route).then(response => response.json());
+        if (data.success) {
+            this.setState({tasks: data.tasks});
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <nav class="bg-primary navbar navbar-dark navbar-expand-lg sticky-top">
+                  <div class="container">
+                    <a class="navbar-brand" href="/">Tasks</a>
+                  </div>
+                </nav>
+                <div class="container mt-4" id="root">
+                    <div class="card-columns">
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
